952d402958a4d496600200aedf158c4463cde5b1
Thierry Damiba <thierry.damiba@gmail.com>
2024-08-11 15:26:57 -0700

    Update utils.py
    add diff models

--- utils.py
+++ utils.py
@@ -9,11 +9,10 @@ from langchain_community.embeddings.fastembed import FastEmbedEmbeddings
 
 logger = get_logger('Langchain-Chatbot')
 
-#decorator
+# decorator
 def enable_chat_history(func):
     if os.environ.get("OPENAI_API_KEY"):
-
-        # to clear chat history after swtching chatbot
+        # to clear chat history after switching chatbot
         current_page = func.__qualname__
         if "current_page" not in st.session_state:
             st.session_state["current_page"] = current_page
@@ -24,20 +23,17 @@ def enable_chat_history(func):
                 del st.session_state["messages"]
             except:
                 pass
-
         # to show chat history on ui
         if "messages" not in st.session_state:
             st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
         for msg in st.session_state["messages"]:
             st.chat_message(msg["role"]).write(msg["content"])
-
     def execute(*args, **kwargs):
         func(*args, **kwargs)
     return execute
 
 def display_msg(msg, author):
     """Method to display message on the UI
-
     Args:
         msg (str): message to display
         author (str): author of the message -user/assistant
@@ -56,14 +52,12 @@ def choose_custom_openai_key():
         st.error("Please add your OpenAI API key to continue.")
         st.info("Obtain your key from this link: https://platform.openai.com/account/api-keys")
         st.stop()
-
     model = "gpt-4o-mini"
     try:
         client = openai.OpenAI(api_key=openai_api_key)
         available_models = [{"id": i.id, "created":datetime.fromtimestamp(i.created)} for i in client.models.list() if str(i.id).startswith("gpt")]
         available_models = sorted(available_models, key=lambda x: x["created"])
         available_models = [i["id"] for i in available_models]
-
         model = st.sidebar.selectbox(
             label="Model",
             options=available_models,
@@ -85,7 +79,6 @@ def configure_llm():
         options=available_llms,
         key="SELECTED_LLM"
         )
-
     if llm_opt == "llama3.1:8b":
         llm = ChatOllama(model="llama3.1", base_url=st.secrets["OLLAMA_ENDPOINT"])
     elif llm_opt == "gpt-4o-mini":
@@ -101,7 +94,13 @@ def print_qa(cls, question, answer):
 
 @st.cache_resource
 def configure_embedding_model():
-    embedding_model = FastEmbedEmbeddings(model_name="BAAI/bge-small-en-v1.5")
+    available_embedding_models = ["BAAI/bge-small-en-v1.5", "BAAI/bge-small-zh-v1.5"]
+    selected_model = st.sidebar.selectbox(
+        label="Embedding Model",
+        options=available_embedding_models,
+        key="SELECTED_EMBEDDING_MODEL"
+    )
+    embedding_model = FastEmbedEmbeddings(model_name=selected_model)
     return embedding_model
 
 def sync_st_session():
